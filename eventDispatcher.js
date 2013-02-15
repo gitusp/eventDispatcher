c58e2fe5f69680dcf33d1cdc3c61d02647f96873
usp <ususso@gmail.com>
2013-02-15 21:03:40 +0900

    refactor and mod fn name

--- eventDispatcher.js
+++ eventDispatcher.js
@@ -4,31 +4,23 @@ define(function () {
          *  register event
          */
         on: function (event, fn) {
-            if (!this._events[event]) {
-                this._events[event] = [];
-            }
-            if (!this.has(event, fn)) {
+            this._events[event] = this._events[event] || [];
+            if ( ! this.has(event, fn)) {
                 this._events[event].push(fn);
             }
         },
 
         /**
-         *  register event
+         *  unregister event
          */
-        un: function (event, fn) {
-            if (!this._events[event]) {
-                return false;
-            }
-            this._events[event] = $.grep(this._events[event], function (elm) {
-                if (elm == fn) {
-                    return false;
-                }
-                return true;
-            });
+        off: function (event, fn) {
+            this._events[event] && (this._events[event] = $.grep(this._events[event], function (elm) {
+                return elm != fn;
+            }));
         },
 
         /**
-         *  register event
+         *  check event
          */
         has: function (event, fn) {
             return this._events[event] && $.inArray(fn, this._events[event]) != -1;
@@ -38,11 +30,9 @@ define(function () {
          *  fire
          */
         fire: function (event, data) {
-            if (!this._events[event]) {
-                return;
-            }
-            $.each(this._events[event], function (i, fn) {
-                fn(data);
+            var that = this;
+            this._events[event] && $.each(this._events[event], function (i, fn) {
+                fn.call(that, data);
             });
         }
     });
